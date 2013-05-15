7937421267c7752b94b99a11e3c3f7b777ed40a9
Jonathan Reed <jdreed@mit.edu>
2013-05-15 13:13:02 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="PyMagtek",
+    version="1.0",
+    description="PyMagtek - Python bindings for MagTek USB readers",
+    author="Jonathan Reed",
+    author_email="jdreed@mit.edu",
+    license="GPL2",
+    py_modules=['magtek'],
+)
